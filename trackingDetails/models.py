68804c6c64d45332c3389e2e07e9874184d604bf
Shibin <sshibinthomass@gmail.com>
2022-04-16 18:02:11 +0530

    initial commit

---
+++ trackingDetails/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+class trackers(models.Model):
+    empId=models.CharField(max_length=100,default='EMP0001')
+    lastUpdatedBy = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
